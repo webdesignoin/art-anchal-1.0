9489e71b8e9d01f5f9e2ec7e5d7d28a8cb7a33e9
Sumit Kumar Yadav <sumit1996926@gmail.com>
2026-05-27 10:10:45 +0530

    clean: remove all local test and patch scripts & add walkthrough.md to track codebase changes

--- test_fetch_orders.cjs
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-let content = fs.readFileSync('src/components/pages/UserProfileView.tsx', 'utf8');
-
-if (content.includes('supabase.auth.onAuthStateChange')) {
-  console.log("Listener exists!");
-} else {
-  console.log("Listener missing!");
-}
